f5b533079e47e809b36bcf208dc318608c07ddbc
Dipak Bari <dipakbari4@gmail.com>
2022-03-01 12:17:06 +0530

    [Added] UserServiceImpl.java

---
+++ src/main/java/com/dipak/mobileappws/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.dipak.mobileappws.service.impl;
+
+import com.dipak.mobileappws.service.UserService;
+import com.dipak.mobileappws.shared.dto.UserDto;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDto createUser(UserDto user) {
+
+        return null;
+    }
+
+}
