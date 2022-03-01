89570f9303001423529255ec9cf282594cf8d68d
Dipak Bari <dipakbari4@gmail.com>
2022-03-01 12:18:58 +0530

    [Added] UserRepository.java

---
+++ src/main/java/com/dipak/mobileappws/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dipak.mobileappws;
+
+import com.dipak.mobileappws.io.entity.UserEntity;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}
